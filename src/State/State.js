e31f50912b26f5174b73e3aaac47e60e91f23afe
CodeMonkey <dmsqlctnekf@gmail.com>
2019-12-04 08:39:26 +0900

    Component Life Cycle

--- src/State/State.js
+++ src/State/State.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Counter from './Counter';
 
 class State extends React.Component {
+    constructor(props) {
+        super(props);
+    }
     state = {
-        count: 0
+        isLoading: true,
+        movies: []
     };
-    add = () => {
-      console.log("add");
-      //this.state.count = 1;
-      //this.setState({count: this.state.count + 1});
-      this.setState(current => ({count: current.count + 1}));
-    };
-    minus = () => {
-      console.log("minus");
-      //this.setState({count: this.state.count - 1});
-      this.setState(current => ({count: current.count - 1}));
-    };
+
+    /*
+
+    **** Component Life Cycle ****
+
+    componentDidMount() {
+      console.log("component Rrendered");
+    }
+    componentDidUpdate() {
+      console.log("I just Updated");
+    }
+    componentWillUnmount() {
+      console.log("Good Bye, Cruel World");
+    }
+    */
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ isLoading: false, book: true })
+        }, 6000)
+    }
+
     render() {
-      return (
-      <div>
-        <h1>The number is { this.state.count }</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-      );
+        const {isLoading} = this.state;
+        console.log("I'm Rendering");
+        return (
+            <div>
+                <div>{ isLoading ? "Loading" : "We are Ready" }</div>
+                <br />
+                {/*<Counter />*/}
+            </div>
+        );
     }
 }
 
 export default State;
+
